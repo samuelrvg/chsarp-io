27a24315bbc2bc664e61ac9294166fe9b14d197e
Samuel Vaz <samuel.rvg@gmail.com>
2019-09-01 22:19:34 -0300

    using stream writer

--- IO/Program.cs
+++ IO/Program.cs
@@ -5,12 +5,15 @@ namespace IO
     class Program
     {
         public static string caminhoArquivo = @"C:\Users\TOTEMTI\Desktop\csharp-projets\CSharpProjets\IO\texto.txt";
+        public static string caminhoArquivoCriar = @"C:\Users\TOTEMTI\Desktop\csharp-projets\CSharpProjets\IO\texto2.txt";
 
         static void Main(string[] args)
         {
             //new UsingFileStream().FileStream(caminhoArquivo);
 
-            new UsingStreamReader().StreamReader(caminhoArquivo);
+            //new UsingStreamReader().StreamReader(caminhoArquivo);
+
+            new UsingStreamWrite().StreamWrite(caminhoArquivoCriar);
 
             Console.ReadKey();
         }
---
+++ IO/UsingStreamWrite.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Text;
+
+namespace IO
+{
+    public class UsingStreamWrite
+    {
+        public void StreamWrite(string caminhoArquivo)
+        {
+            using (FileStream fs = new FileStream(caminhoArquivo, FileMode.CreateNew))
+            using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+            {
+                sw.Write("texto de teste stream writer");
+            }
+        }
+    }
+}
